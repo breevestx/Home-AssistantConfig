8d6689d5dc7eda93291ddbffa311e7f813a603c1
breevestx <breeves@nexacomm.com>
2017-08-04 16:18:39 -0500

    Minor edits and clean up

--- shell/backup_config.sh
+++ shell/backup_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /tmp && zip -r "Home-Assistant-Backup-$(date +"%Y-%m-%d").zip" /home/homeassistant/.homeassistant/ -x "*.log" -x "/*tts/*" -x "*.db" -x "*.db-shm" -x "*.db-wal" -x "*.mp3" -x "/*deps/*" -x "/*www/camera_shots/*" -x "/*.git/*" -x "/*shell/gif_maker/venv/*"
+cd /home/pi/backups/ && zip -r "Home-Assistant-Backup-$(date +"%Y-%m-%d").zip" /home/homeassistant/.homeassistant/ -x "*.log" -x "/*tts/*" -x "*.db" -x "*.db-shm" -x "*.db-wal" -x "*.mp3" -x "/*deps/*" -x "/*www/camera_shots/*" -x "/*.git/*" -x "/*shell/gif_maker/venv/*"
 
